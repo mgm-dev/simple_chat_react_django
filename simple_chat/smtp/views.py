e4a9705f787e8a6c2963851b7395d23a9d88affc
Min Gyeong-Min <tommin231@gmail.com>
2020-09-07 21:17:40 +0900

    created simple smtp backend

--- simple_chat/smtp/views.py
+++ simple_chat/smtp/views.py
@@ -10,4 +10,11 @@ class IndexView(View):
         if request.META['CONTENT_TYPE'] == "application/json":
             print(request.body.decode('UTF-8'))
             requestJson = json.loads(request.body.decode('UTF-8'))
-            return JsonResponse(requestJson)
+            send_mail(
+                'Simple Chat ' + requestJson['subject'],
+                requestJson['from'] + '\n' + requestJson['message'],
+                'tommin231@gmail.com',
+                ['tommin231@gmail.com'],
+                fail_silently=False,
+            )
+            return HttpResponse(status=200)
